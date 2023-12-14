d87e110d3db5972e55df5c509b61b0173d51d281
Peter Lewerin <plewerin@gmail.com>
2023-12-14 19:46:30 +0100

    a basic map, and moving around on it

--- main.c
+++ main.c
@@ -1,114 +1,154 @@
 // https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/index.html
 #include <ncurses.h>
 
+WINDOW *create_newwin(int height, int width, int starty, int startx) {
+    WINDOW *local_win;
+
+    local_win = newwin(height, width, starty, startx);
+    box(local_win, 0 , 0);
+    wrefresh(local_win);
+
+    return local_win;
+}
+
 int main()
 {	
 	initscr();
 	cbreak();
-	keypad(stdscr, TRUE);
 	noecho();
 
-    curs_set(0);
+    intrflush(stdscr, FALSE);
+	keypad(stdscr, TRUE);
+
     refresh();
+
+    int height = 16;
+    int width = 20;
+    int starty = (LINES - height) / 2;  /* Calculating for a center placement */
+    int startx = (COLS - width) / 2;
+    WINDOW *room = create_newwin(height, width, starty, startx);
+    keypad(room, TRUE);
+
+    curs_set(0);
     int x = 10;
     int y =  5;
-    mvaddch(y, x, '@');
+    mvwaddch(room, y, x, '@');
+    wrefresh(room);
     int ch;
+    chtype floor;
     do {
-        ch = getch();
-        mvaddch(y, x, ' ');
-        if(ch == '1')
-            mvaddch(++y, --x, '@');
-        else if(ch == '2')
-            mvaddch(++y, x, '@');
-        else if(ch == '3')
-            mvaddch(++y, ++x, '@');
-        else if(ch == '4')
-            mvaddch(y, --x, '@');
-        else if(ch == '5')
-            mvaddch(y, x, '@');
-        else if(ch == '6')
-            mvaddch(y, ++x, '@');
-        else if(ch == '7')
-            mvaddch(--y, --x, '@');
-        else if(ch == '8')
-            mvaddch(--y, x, '@');
-        else if(ch == '9')
-            mvaddch(--y, ++x, '@');
-        else if(ch == 'c') {
-            mvaddch(y, x, '@');
+        ch = wgetch(room);
+        if(ch == '1') {
+            floor = mvwinch(room, y+1, x-1);
+            if (floor == ' ') {
+                mvwaddch(room, y, x, ' ');
+                mvwaddch(room, ++y, --x, '@');
+            }
+        } else if(ch == '2') {
+            floor = mvwinch(room, y+1, x);
+            if (floor == ' ') {
+                mvwaddch(room, y, x, ' ');
+                mvwaddch(room, ++y, x, '@');
+            }
+        } else if(ch == '3') {
+            floor = mvwinch(room, y+1, x+1);
+            if (floor == ' ') {
+                mvwaddch(room, y, x, ' ');
+                mvwaddch(room, ++y, ++x, '@');
+            }
+        } else if(ch == '4') {
+            floor = mvwinch(room, y, x-1);
+            if (floor == ' ') {
+                mvwaddch(room, y, x, ' ');
+                mvwaddch(room, y, --x, '@');
+            }
+        } else if(ch == '5') {
+            mvwaddch(room, y, x, '@');
+        } else if(ch == '6') {
+            floor = mvwinch(room, y, x+1);
+            if (floor == ' ') {
+                mvwaddch(room, y, x, ' ');
+                mvwaddch(room, y, ++x, '@');
+            }
+        } else if(ch == '7') {
+            floor = mvwinch(room, y-1, x-1);
+            if (floor == ' ') {
+                mvwaddch(room, y, x, ' ');
+                mvwaddch(room, --y, --x, '@');
+            }
+        } else if(ch == '8') {
+            floor = mvwinch(room, y-1, x);
+            if (floor == ' ') {
+                mvwaddch(room, y, x, ' ');
+                mvwaddch(room, --y, x, '@');
+            }
+        } else if(ch == '9') {
+            floor = mvwinch(room, y-1, x+1);
+            if (floor == ' ') {
+                mvwaddch(room, y, x, ' ');
+                mvwaddch(room, --y, ++x, '@');
+            }
+        } else if(ch == 'c') {
             mvaddstr(1, 0, "close what?");
             clrtoeol();
         } else if(ch == 'd') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "drop what?");
             clrtoeol();
         } else if(ch == 'e') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "eat what?");
             clrtoeol();
         } else if(ch == 'E') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "equip what?");
             clrtoeol();
         } else if(ch == 'g') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "get what?");
             clrtoeol();
         } else if(ch == 'i') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "inventory");
             clrtoeol();
         } else if(ch == 'l') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "look in which direction?");
             clrtoeol();
         } else if(ch == 'q') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "drink what?");
             clrtoeol();
         } else if(ch == 'r') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "read what?");
             clrtoeol();
         } else if(ch == 'u') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "use what?");
             clrtoeol();
         } else if(ch == 'w') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "wear what?");
             clrtoeol();
         } else if(ch == 'z') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "zap in which direction?");
             clrtoeol();
         } else if(ch == ',') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "picked up <item>");
             clrtoeol();
         } else if(ch == '<') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "you climbed a stair");
             clrtoeol();
         } else if(ch == '>') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "you went down a stair");
             clrtoeol();
         } else if(ch == '@') {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "character screen");
             clrtoeol();
         } else if(ch == KEY_F(1)) {
-            mvaddch(y, x, '@');
             mvaddstr(1, 0, "help screen");
             clrtoeol();
-        // more keys at roguebasin
+            // more keys at roguebasin
         } else 
-            mvaddch(y, x, '@');
-        refresh();			/* Print it on to the real screen */
+            ;
+
+        refresh();
+        wrefresh(room);
     } while (ch != 'Q');
-	endwin();			/* End curses mode		  */
+
+    delwin(room);
+	endwin();
 
 	return 0;
 }
