960c7ce91fbd361e46a5edf6bb9489161f20301f
Peter Lewerin <plewerin@gmail.com>
2023-12-24 20:41:37 +0100

    added action handlers for some more command keys

--- ah.c
+++ ah.c
@@ -1,19 +1,51 @@
 #include "main.h"
 #include "ah.h"
 
+/* This source code contains fragments of the Rogue 3.6.3 source code (noted in comment), which is
+
+    Copyright (C) 2005 Nicholas J. Kisseberth
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+    3. Neither the name(s) of the author(s) nor the names of other contributors
+       may be used to endorse or promote products derived from this software
+       without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) AND CONTRIBUTORS ``AS IS'' AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR(S) OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+    OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+    OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+    SUCH DAMAGE.
+*/
+
+/* Except for such fragments, this code is Copyright (C) 2023 Peter Lewerin */
+
 ahptr actionHandlers[256] = {
    /*0x*/&ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, 
-   /*1x*/&ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ahCS, 
+   /*1x*/&ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, 
    /*2x*/&ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, 
    /*3x*/&ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, 
    /*4x*/&ah__, &ah__, &ah__, &ah__, &ah_p, &ah__, &ah_d, &ah__, &ah__, &ah_1,
    /*5x*/&ah_2, &ah_3, &ah_4, &ah_5, &ah_6, &ah_7, &ah_8, &ah_9, &ah__, &ah__,
-   /*6x*/&ah_S, &ah__, &ah_s, &ah_h, &ah__, &ah__, &ah__, &ah__, &ah__, &ah_E, 
-   /*7x*/&ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, 
-   /*8x*/&ah__, &ah__, &ah__, &ah_S, &ah__, &ah__, &ah__, &ah_W, &ah__, &ah__, 
+   /*6x*/&ah__, &ah__, &ah__, &ah_h, &ah__, &ah__, &ah__, &ah__, &ah__, &ah_E, 
+   /*7x*/&ah__, &ah__, &ah__, &ah_I, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, 
+   /*8x*/&ah_P, &ah_Q, &ah_R, &ah_S, &ah_T, &ah__, &ah__, &ah_W, &ah__, &ah__, 
    /*9x*/&ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah_c, 
-  /*10x*/&ah_d, &ah_e, &ah__, &ah_g, &ah_h, &ah_i, &ah__, &ah__, &ah_l, &ah__, 
-  /*11x*/&ah__, &ah__, &ah_p, &ah_q, &ah_r, &ah_s, &ah__, &ah_u, &ah__, &ah_w, 
+  /*10x*/&ah_d, &ah_e, &ah_r, &ah_g, &ah_h, &ah_i, &ah__, &ah__, &ah_l, &ah__, 
+  /*11x*/&ah__, &ah__, &ah_p, &ah_q, &ah_r, &ah_s, &ah_t, &ah_u, &ah__, &ah_w, 
   /*12x*/&ah__, &ah__, &ah_z, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, 
   /*13x*/&ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, 
   /*14x*/&ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, &ah__, 
@@ -171,15 +203,88 @@ void ah_E(THING* sprite) {
     }
 }
 
+void ah_f(THING* sprite) {
+    (void)sprite;
+    //TODO
+    msg("forward until reach something");
+    clrtoeol();
+}
+
 void ah_g(THING* sprite) {
     (void)sprite;
     msg("get what?");
     clrtoeol();
 }
 
+// help string apparatus copied from Rogue3.6.3 source.
+// it was just too pretty to resist.
+struct h_list {
+    char h_ch;
+    char *h_desc;
+};
+
+// edited from Rogue3.6.3
+struct h_list helpstr[] = {
+    { '?',	",h,F1\tprints help" },
+    { '/',	"\tidentify object" },
+    { 't',	"<dir>\tthrow something" },
+    { 'f',	"<dir>\tforward until find something" },
+    { 'p',	"<dir>\tzap a wand in a direction" },
+    { 'z',	"\tzap a wand or staff" },
+    { '>',	"\tgo down a staircase" },
+    { 's',	"\tsearch for trap/secret door" },
+    { '5',	"\trest for a while" },
+    { 'i',	"\tinventory" },
+    { 'q',	"\tquaff potion" },
+    { 'r',	"\tread paper" },
+    { 'e',	"\teat food" },
+    { 'w',	"\twield a weapon" },
+    { 'W',	"\twear armor" },
+    { 'T',	"\ttake armor off" },
+    { 'P',	"\tput on ring" },
+    { 'R',	"\tremove ring" },
+    { 'd',	"\tdrop object" },
+    { 'S',	"\tsave game" },
+    { 'Q',	"\tquit" },
+    { 0, 0 }
+};
+
 void ah_h(THING* sprite) {
+    // show a single help line, or all of them
+    // adapted from the code in Rogue 3.6.3
     (void)sprite;
-    msg("help screen");
+    register struct h_list *strp = helpstr;
+    msg("Character you want help for (* for all): ");
+    chtype ch = getch();
+    if (ch != '*') {
+        move(0, 0);
+        while (strp->h_ch) {
+            if (strp->h_ch == (char)ch) {
+                printw("%c%s", strp->h_ch, strp->h_desc);
+                clrtoeol();
+                refresh();
+                break;
+            }
+            strp++;
+        }
+        if (strp->h_ch != (char)ch)
+            printw("Unknown character '%c'", ch);
+        return;
+    }
+    WINDOW* hw = create_newwin(22, 30, 2, 72);
+    wclear(hw);
+    int cnt = 0;
+    while (strp->h_ch) {
+        mvwaddch(hw, cnt, 0, strp->h_ch);
+        waddstr(hw, strp->h_desc);
+        cnt++;
+        strp++;
+    }
+    wrefresh(hw);
+    ch = wgetch(hw);
+    werase(hw);
+    destroy_win(hw);
+    move(0, 0);
     clrtoeol();
 }
 
@@ -259,13 +364,57 @@ void ah_r(THING* sprite) {
 
 void ah_s(THING* sprite) {
     (void)sprite;
-    msg("you went down a stair");
+    //TODO search for hidden things, using sprite for position
+    msg("search hidden");
+    clrtoeol();
+}
+
+void ah_t(THING* sprite) {
+    (void)sprite;
+    //TODO search for hidden things, using sprite for position
+    msg("throw something");
+    clrtoeol();
+}
+
+void ah_I(THING* sprite) {
+    (void)sprite;
+    //TODO
+    msg("identify object (also /)");
+    clrtoeol();
+}
+
+void ah_P(THING* sprite) {
+    (void)sprite;
+    //TODO remove a ring, if possible
+    msg("put on a ring");
+    clrtoeol();
+}
+
+void ah_Q(THING* sprite) {
+    (void)sprite;
+    //TODO 
+    msg("quit game");
+    clrtoeol();
+}
+
+void ah_R(THING* sprite) {
+    (void)sprite;
+    //TODO remove a ring, if possible
+    msg("remove a ring");
     clrtoeol();
 }
 
 void ah_S(THING* sprite) {
     (void)sprite;
-    msg("you went up a stair");
+    //TODO
+    msg("save game");
+    clrtoeol();
+}
+
+void ah_T(THING* sprite) {
+    (void)sprite;
+    //TODO take off armour, if possible
+    msg("remove armour");
     clrtoeol();
 }
 
@@ -345,15 +494,17 @@ void ah_z(THING* sprite) {
     chtype ch = endPopup(invlist);
     int i = ch-'a';
     if (allowedIndices[i]) {
-        // TODO find a target as well
+        // this is targetless zap
         zapEffect(i);
     } else {
-        msg("you can't wear that"); clrtoeol(); refresh();
+        msg("you can't zap with that"); clrtoeol(); refresh();
     }
 }
 
 void ah_p(THING* sprite) {
-    pickUpObject(sprite);
+    (void)sprite;
+    // TODO use sprite to zap in a direction
+    //pickUpObject(sprite);
 }
 
 void ah_d(THING* sprite) {
--- ah.h
+++ ah.h
@@ -14,7 +14,8 @@ void ah_6(THING*); // 6 e
 void ah_7(THING*); // 7 nw
 void ah_8(THING*); // 8 n
 void ah_9(THING*); // 9 ne
-void ah_E(THING*); // E equip
+void ah_E(THING*); // E equip*
+void ah_I(THING*); // I identify object (also /)
 void ah_P(THING*); // P put on ring
 void ah_Q(THING*); // Q quit game
 void ah_R(THING*); // R remove ring
@@ -24,6 +25,7 @@ void ah__(THING*); // _ null action
 void ah_c(THING*); // c close smth*
 void ah_d(THING*); // d drop, also .
 void ah_e(THING*); // e eat
+void ah_f(THING*); // f forward until reach something
 void ah_g(THING*); // g get*
 void ah_h(THING*); // h help screen, also ? and F1
 void ah_i(THING*); // i inventory
@@ -32,6 +34,7 @@ void ah_p(THING*); // p zap/dir
 void ah_q(THING*); // q quaff
 void ah_r(THING*); // r read
 void ah_s(THING*); // s search for hidden things
+void ah_t(THING*); // t throw something
 void ah_u(THING*); // u use*
 void ah_w(THING*); // w wield
 void ah_W(THING*); // W wear
